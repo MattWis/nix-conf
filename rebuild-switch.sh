1e25461edb4f34162935a8f244e77b830723c875
Matt Wismer <mattwis86@gmail.com>
2024-03-11 21:23:53 -0700

    10 current  2024-03-11 21:15:54  23.11.5195.2be119add7b3  6.1.81                          *

--- rebuild-switch.sh
+++ rebuild-switch.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
 
+# Copy local config to global config and rebuild
 sudo cp `dirname $0`/configuration.nix /etc/nixos
 sudo nixos-rebuild switch
+
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep current)
+
+# Commit all changes witih the generation metadata
+git commit -am "$current"
