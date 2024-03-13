d092361afb493179ed86edd536536d1e5a366e60
Matt Wismer <mattwis86@gmail.com>
2024-03-12 21:40:09 -0700

    3 current   2024-03-12 21:38:33  23.11.5195.2be119add7b3  6.1.81                          *

--- configuration.nix
+++ configuration.nix
@@ -8,23 +8,10 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      # Allow machine-specific bootloader settings
+      ./bootloader.nix
     ];
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
-  boot.initrd.luks.devices."luks-6f91acd4-b846-4c17-8d46-fa68486c957f".device = "/dev/disk/by-uuid/6f91acd4-b846-4c17-8d46-fa68486c957f";
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
-
-  boot.loader.grub.enableCryptodisk=true;
-
-  boot.initrd.luks.devices."luks-c39762f6-1e18-459b-9f7f-dc4793a88e66".keyFile = "/crypto_keyfile.bin";
-  boot.initrd.luks.devices."luks-6f91acd4-b846-4c17-8d46-fa68486c957f".keyFile = "/crypto_keyfile.bin";
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
