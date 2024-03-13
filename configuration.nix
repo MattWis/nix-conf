ade7bedb26a6024317ea1e7147390d3ea2752161
Matt Wismer <mattwis86@gmail.com>
2024-03-12 18:49:15 -0700

    16 current  2024-03-12 18:49:12  23.11.5195.2be119add7b3  6.1.81                          *

--- configuration.nix
+++ configuration.nix
@@ -160,8 +160,8 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
-  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+  networking.firewall.allowedTCPPorts = [ 8384 17500 22000 ];
+  networking.firewall.allowedUDPPorts = [ 17500 22000 21027 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
