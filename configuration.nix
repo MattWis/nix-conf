a506dde2aeb5593619c77fb85db0fe7004ff3d4e
Matt Wismer <mattwis86@gmail.com>
2024-06-04 22:10:01 -0400

    timeframenix: 12 current  2024-06-04 22:09:58  23.11.5474.fa9f817df522  6.1.82                          *

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,11 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   sound.enable = true;
