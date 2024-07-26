7e7e587b5d9b8a4f557fbd8be4ed1da7a6467b33
Matt Wismer <mattwis86@gmail.com>
2024-07-25 21:40:31 -0400

    timeframenix: 16 current  2024-07-25 21:40:21  23.11.5474.fa9f817df522  6.1.82                          *

--- configuration.nix
+++ configuration.nix
@@ -136,6 +136,10 @@
     defaultEditor = true;
   };
 
+  programs.steam = {
+    enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
