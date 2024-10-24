a74e5d934d022e19e4902f90aef276bd8d6ed930
Matt Wismer <mattwis86@gmail.com>
2024-10-24 00:26:29 -0400

    timeframenix: 41 current  2024-10-24 00:26:23  24.05.5860.89172919243d  6.6.57                          *

--- configuration.nix
+++ configuration.nix
@@ -112,6 +112,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    acpi
     alacritty
     anki-bin
     cdrtools
