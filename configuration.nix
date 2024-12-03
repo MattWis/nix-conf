0ff9dbcc27d1d3af02b1182825fd891c753584fe
Matt Wismer <mattwis86@gmail.com>
2024-12-03 17:50:40 -0500

    timeframenix: 59 current  2024-12-03 17:50:31  24.11.710194.f9f0d5c5380b  6.6.63                          *
    Update to 2024.11

--- configuration.nix
+++ configuration.nix
@@ -64,8 +64,6 @@
     openFirewall = true;
   };
 
-  # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
