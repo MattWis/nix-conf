45917768e51d9f448dc964a290fab441dd4e02d0
MattWis <mattwis86@gmail.com>
2025-03-28 16:13:53 -0400

    timelin: 18 current  2025-03-28 16:13:45  23.11.7870.205fd4226592  6.1.96                          *

--- timelin/machine-specific.nix
+++ timelin/machine-specific.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
