aecb56b5b40ca73e17f3bce4a419c65d31b8bb70
Matt Wismer <mattwis86@gmail.com>
2025-03-28 15:50:26 -0400

    --no-edit

--- nix.sh
+++ nix.sh
@@ -7,7 +7,7 @@ nvim configuration.nix
 # Copy local config to global config and rebuild
 sudo cp `hostname`/*.nix /etc/nixos
 sudo cp configuration.nix /etc/nixos
-sudo nixos-rebuild switch --cores 4 --upgrade || exit 1
+sudo nixos-rebuild switch --upgrade || exit 1
 
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current)
