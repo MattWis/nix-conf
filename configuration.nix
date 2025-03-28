d9bde874d3b2228cfa3c30d87973ead78d63543d
Matt Wismer <mattwis86@gmail.com>
2025-03-28 16:01:46 -0400

    timeframenix: 89 current  2025-03-28 16:01:42  24.11.716288.d02d88f8de5b  6.6.84                          *

--- configuration.nix
+++ configuration.nix
@@ -55,14 +55,6 @@
     xkb.variant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
